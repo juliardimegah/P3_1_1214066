d3869389f1fd1c90c72d295c67004c853d8392c9
Megah Juliardi <Juliardimegah@outlook.com>
2022-10-03 16:02:50 +0700

    Add project files.

---
+++ P3_1_1214066/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_1_1214066
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan nilai n  ");
+            var jumanakayam = int.Parse(Console.ReadLine());
+            var jumanakayam2 = jumanakayam - 1;
+            Console.WriteLine(" ");
+
+            if (jumanakayam>=1 && jumanakayam<=10) {
+                for (int i = jumanakayam; i > 0; i--)
+                {
+                    if (i > 1)
+                    {
+                        var j = i - 1;
+                        Console.WriteLine("Anak ayam turunlah " + i + ", mati satu tinggallah " + j);
+                    }
+                    else { Console.WriteLine("Anak ayam turunlah 1, mati satu tinggallah induknya"); }
+                }
+            } else { Console.WriteLine("Nilai n tidak valid"); }
+        }
+    }
+}
